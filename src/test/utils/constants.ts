63ae63a4243370484089d3d610bd4db735de6921
Ranganath L <ranganath.l.2001@gmail.com>
2025-03-18 18:48:22 +0530

    updated constants file

--- src/test/utils/constants.ts
+++ src/test/utils/constants.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config({});
+
 export const key = String(process.env.TRELLO_API_KEY); // Replace with your API key
 export const token = String(process.env.TRELLO_API_TOKEN); // Replace with your API token
 export const trelloBoardId = String(process.env.TRELLO_BOARD_ID);
