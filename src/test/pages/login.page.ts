06ebb31ccbbf3fd3b36cd8a447cd8c892464ef5b
Ranganath-L_1701 <ranganath.l.2001@gmail.com>
2025-05-20 15:53:42 +0530

    investing the ci failure

--- src/test/pages/login.page.ts
+++ src/test/pages/login.page.ts
@@ -38,7 +38,7 @@ export class LoginPage extends BasePage {
         await this.page.fill(this.passwordInput, trelloLoginPassword);
         await this.page.click(this.submitLoginBtn);
 
-        await this.page?.waitForTimeout(1000);
+        await this.page?.waitForTimeout(1500);
         await this.page?.getByRole('textbox', { name: '-digit verification code' }).click();
         // Wait for a short period to ensure TOTP is valid for the current window
         await this.page.waitForTimeout(1500);
