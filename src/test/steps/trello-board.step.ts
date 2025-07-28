8b26cffeca0151e5f7888953ba9bcf5d6053b3b2
Ranganath-L_1701 <ranganath.l.2001@gmail.com>
2025-07-28 15:52:26 +0530

    test ci issue

--- src/test/steps/trello-board.step.ts
+++ src/test/steps/trello-board.step.ts
@@ -21,6 +21,7 @@ Then('Create the lists', async function (this: ICustomWorld) {
 });
 
 When('Switch to Project Workspace', async function (this: ICustomWorld) {
+    await new Promise(resolve => setTimeout(resolve, 3000));
     await this.page?.screenshot({ path: 'screenshots/failure.png' });
     await this.page?.waitForURL('**/boards', { timeout: 10000 });
     await this.pagesObj?.boardPage.switchToProjectWorkspace();
