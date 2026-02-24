618e184b664fdf7ecc925c315e3d76ef0ef91cac
Ranganath L <75678072+ranganath2602@users.noreply.github.com>
2026-02-24 18:51:28 +0530

    Update browser launch settings in trello-login step
    Simplified browser launch configuration for tests.

--- src/test/steps/trello-login.step.ts
+++ src/test/steps/trello-login.step.ts
@@ -14,10 +14,7 @@ const browserInstances: Browser[] = [];
 
 Before(async function (this: ICustomWorld) {
   // Launch a new browser and store it in our global array
-  const newBrowser = await chromium.launch({ 
-    headless: process.env.CI === 'true',
-    args: process.env.CI ? ['--no-sandbox'] : [] 
-  });
+  const newBrowser = await chromium.launch({ headless: true });
   browserInstances.push(newBrowser);
 
   this.context = await newBrowser.newContext();
